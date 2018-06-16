5f777c83a5ae896aa9e3dc942250862ca8d9e16d
Nircek <nircek-2103@protonmail.com>
2018-06-16 17:23:45 +0200

    Add " command execution Add command selecting Repair navigating in file

--- nr4c-editor.py
+++ nr4c-editor.py
@@ -1,4 +1,49 @@
 import sys
+
+
+def cmd(lines, i, shift='', end='\n'):
+    rlines = []
+    f = False
+    while not f:
+        if not i[1] < len(lines[i[0]]):
+            i[0] += 1
+            i[1] = 0
+            break
+        if lines[i[0]][i[1]] == '"':
+            rlines += [ str(shift + lines[i[0]][i[1]+1:] + end) ]
+            i[1] = len(lines[i[0]])
+        else:
+            print('WARN: don\'t know command ', lines[i[0]][i[1]], ', skipping', sep='')
+            i[1] += 1
+    return [rlines, i]
+
+
+def interpreter(lines):
+    rlines = []
+    i = [0, 0]
+    while i[0] < len(lines):
+        if i[0] == 26:
+            print(end='')
+        if len(lines[i[0]]) == 0:
+            rlines += ['\n']
+            i[0] += 1
+        if lines[i[0]][0] == '#':
+            i[0] += 1
+            continue
+        elif lines[i[0]][0] == '/':
+            i[1] = 1
+            r = cmd(lines, i)
+            rlines += r[0]
+            i = r[1]
+        else:
+            rlines += lines[i[0]]
+            i[0] += 1
+    nrlines = []
+    for line in rlines:
+        nrlines += line + '\n'
+    return rlines
+
+
 if __name__ == '__main__':
     try:
         if len(sys.argv) > 1:
@@ -8,12 +53,13 @@ if __name__ == '__main__':
         if len(sys.argv) > 2:
             fo = open(sys.argv[2], 'w')
         else:
-            fo = open(input('OUTPUT FILE:'), 'w')
+            fo = open(input('OUTPUT FILE: '), 'w')
+        fo.writelines(interpreter(fi.read().splitlines()))
     except FileNotFoundError:
         print('This file cannot be found...', file=sys.stderr)
         if len(sys.argv) > 1:
             print('Usage: ', sys.argv[0], ' [INPUT FILE] [OUTPUT FILE]', file=sys.stderr)
-
+    finally:
         if 'fi' in locals():
             fi.close()
         if 'fo' in locals():
