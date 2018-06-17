34a5ce087898d27e1cb698194bb162faa57705ae
Nircek <nircek-2103@protonmail.com>
2018-06-17 16:22:51 +0200

    Add left, right and center alignment commands Update NR4C-3 standard (add . and n command) Refresh " command execution in normal mode to one while loop

--- nr4c-editor.py
+++ nr4c-editor.py
@@ -26,32 +26,43 @@
 # SOFTWARE.
 import sys
 import re
-global width, rlines, mode, i, shift
+global width, rlines, gmode,  mode, i, shift, fline
 width = 80
 rlines = []
+gmode = ''
 mode = ''
 i = [0, 0]
 shift = ['']
-
+last_a = ''
+fline = ''
+# formatting line
 
 
 def cmd(lines):
-    global width, rlines, mode, i, shift
+    global width, rlines, gmode, mode, i, shift, fline
     while True:
         if not i[1] < len(lines[i[0]]):
             i[0] += 1
             i[1] = 0
             break
-        # elif lines[i[0]][i[1]] == 'l':
-        # elif lines[i[0]][i[1]] == 'c':
-        # elif lines[i[0]][i[1]] == 'r':
+        elif lines[i[0]][i[1]] == 'l':
+            mode = 'l'
+            i[1] += 1
+        elif lines[i[0]][i[1]] == 'c':
+            mode = 'c'
+            i[1] += 1
+        elif lines[i[0]][i[1]] == 'r':
+            mode = 'r'
+            i[1] += 1
         elif lines[i[0]][i[1]] == ':':
+            gmode = mode
             k = shift
             shift = ['']
             for j in range(len(k)):
                 shift.append(k[j])
             i[1] += 1
         elif lines[i[0]][i[1]] == 'e':
+            gmode = ''
             k = shift
             shift = []
             for j in range(1, len(k)):
@@ -64,29 +75,63 @@ def cmd(lines):
         elif lines[i[0]][i[1]] == '-':
             shift[0] += ' - '
             i[1] += 1
+        elif lines[i[0]][i[1]] == '.':
+            rlines += [fline+'\n']
+            fline = ''
+            i[1] += 1
         elif lines[i[0]][i[1]] == '"':
+            m = gmode
+            if mode != '':
+                m = mode
             s = ''
             for j in shift:
                 s = j + s
-            e = s + lines[i[0]][i[1]+1:]
-            while len(e) > width:
-                c = e.rfind(' ', 0, 80)
-                rlines += [e[:c]+'\n']
-                # delete a) - etc.
-                e = re.compile('[a-zA-Z0-9_)(-]').sub(' ', s) + e[c+1:]
-            rlines += [e]
-            rlines[-1] += '\n'
+            w = width - len(s)
+            e = lines[i[0]][i[1]+1:]
+            if m == '':
+                while e != '':
+                    e = s + e
+                    if len(e) > width:
+                        c = e.rfind(' ', 0, 80)
+                    else:
+                        c = len(e)
+                    rlines += [e[:c]+'\n']
+                    for k in range(len(shift)):
+                        shift[k] = re.compile('[a-zA-Z0-9_)(-]').sub(' ', shift[k])
+                    s = ''
+                    for j in shift:
+                        s = j + s
+                    e = e[c+1:]
+            else:
+                if fline == '':
+                    fline = s + ' '*w
+                    for k in range(len(shift)):
+                        shift[k] = re.compile('[a-zA-Z0-9_)(-]').sub(' ', shift[k])
+                ml = len(e)
+                if m == 'l':
+                    mf = len(s)
+                    me = len(s) + ml
+                elif m == 'r':
+                    mf = width - ml
+                    me = width
+                elif m == 'c':
+                    mf = len(s) + (w-ml)//2
+                    me = mf + ml
+                if fline[mf:me] != ml * ' ':
+                    print('WARN(', i, '): covering \'', fline[mf:me], '\' by \'', e, '\'', sep='')
+                fline = fline[:mf] + e + fline[me:]
             i[1] = len(lines[i[0]])
             shift[0] = ''
+            mode = ''
         else:
             print('WARN(', i, '): don\'t know command ', lines[i[0]][i[1]], ', skipping', sep='')
             i[1] += 1
 
 
 def interpreter(lines):
-    global width, rlines, mode, i, shift
+    global width, rlines, gmode,  mode, i, shift
     while i[0] < len(lines):
-        if i[0] > 114:
+        if i[0] > 26:
             print(end='')
         if len(lines[i[0]]) == 0:
             rlines += ['\n']
